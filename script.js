a284178cbefbe7e6931e65791f82fe560020d043
Gabriel Brock <31634953+gabibrock@users.noreply.github.com>
2024-06-12 14:54:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     const gift = document.getElementById("gift");
     const surprise = document.getElementById("surprise");
+    const closeButton = document.getElementById("closeButton");
 
     gift.addEventListener("click", function() {
         gift.style.display = "none";
         surprise.classList.remove("hidden");
+    });
+
+    closeButton.addEventListener("click", function() {
+        surprise.classList.add("hidden");
+        gift.style.display = "block";
     });
 });
